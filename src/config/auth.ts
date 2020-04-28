f57f2cebc52814757a3c864e954da444fc934986
Cesar Denis <cesar@cesardenis.com>
2020-04-27 22:08:21 -0300

    feat: add authenticated routes

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: 'aea6e850a44187c0d3443fd03b00875f',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,8 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
+
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +32,11 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    const token = sign({}, 'aea6e850a44187c0d3443fd03b00875f', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
